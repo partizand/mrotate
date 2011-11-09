97a883a821fc0365a092d7897f066656972444e2
partizand <partizand@gmail.com>
2011-11-09 12:54:59 +0400

    Добавлен текст лицензии

--- mrotate/ReplVar.h
+++ mrotate/ReplVar.h
@@ -1,3 +1,26 @@
+/*
+	This file is part of mrotate. Rotate logs for Windows
+
+    Copyright (C) 2011  Andrey Kapustin
+
+    mrotate is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    mrotate is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Код распространяется по лицензии GPL 3
+	Автор: Капустин Андрей, 2011 г.
+
+*/
+
 #pragma once
 
 #include <vector>
